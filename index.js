0f3c707540e325578ccbcbea7832d0aeb95eab4b
Hadi <haditrumboo@gmail.com>
2026-02-27 22:40:11 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-let a = 10;
-let b = 20;
+// let a = 10;
+// let b = 20;
  
-function add (x, y) {
-    return x + y;
-}
+// function add (x, y) {
+//     return x + y;
+// }
 
 console.log(add(a, b));
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({message: 'Hello World!'})
+})
+app.listen(3000);
