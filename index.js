ee0f6e8fb3e2723fabf8e67e020b27b1dfb907d2
Hadi <haditrumboo@gmail.com>
2026-03-14 21:25:25 +0530

    feat: add global error handling middleware

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ function validnumbers(req, res, next) {
     }
     next();  
 }
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        error: "Something went wrong"
+    });
+});
+
 
 app.use(reqcount);
 
